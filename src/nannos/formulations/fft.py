f928232d355a547489cb2dbc3a0bf4856f7919fb
Benjamin Vial <b.vial@qmul.ac.uk>
2022-02-04 18:13:00 +0000

    test gpu

--- src/nannos/formulations/fft.py
+++ src/nannos/formulations/fft.py
@@ -11,15 +11,17 @@ from .. import numpy as np
 
 _device = "cuda" if HAS_CUDA else "cpu"
 _BACKEND = get_backend()
-_fft2 = torch.fft if _BACKEND == "torch" else np.fft.fft2
-_ifft2 = torch.fft if _BACKEND == "torch" else np.fft.ifft2
+if _BACKEND == "torch":
+    from .. import torch
+_fft2 = torch.fft.fft2 if _BACKEND == "torch" else np.fft.fft2
+_ifft2 = torch.fft.ifft2 if _BACKEND == "torch" else np.fft.ifft2
 
 BACKEND = get_backend()
 
 
 def fourier_transform(u):
     if _BACKEND == "torch":
-        u = u.to(_device)
+        u = torch.Tensor(u).to(_device)
     uft = _fft2(u)
     nx, ny = np.shape(uft)[:2]
     return uft / (nx * ny)
@@ -27,7 +29,7 @@ def fourier_transform(u):
 
 def inverse_fourier_transform(uft, shape=None, axes=(-2, -1)):
     if _BACKEND == "torch":
-        u = u.to(_device)
+        uft = torch.Tensor(uft).to(_device)
     u = _ifft2(uft, s=shape, axes=axes)
     nx, ny = np.shape(u)[:2]
     return u * (nx * ny)
---
+++ test/benchmark_fft.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Benjamin Vial
+# This file is part of nannos
+# License: GPLv3
+# See the documentation at nannos.gitlab.io
+
+import numpy as np
+import pytest
+
+
+np.random.seed(84)
+N = 2**10
+matrix_fft = np.random.rand(N, N) + 1j * np.random.rand(N, N)
+
+def test_fft():
+    i = 0
+    vals, vects = [], []
+    for n in [8,10,12]:
+        N = 2**n
+
+        print(f"matrix size = {N}")
+        matrix_fft = np.random.rand(N, N) + 1j * np.random.rand(N, N)
+        for backend in ["autograd", "jax", "torch", "numpy"]:
+            import nannos as nn
+            print("backend: ", backend)
+            nn.set_backend(backend)
+            t = nn.tic()
+            utf = nn.formulations.fft.fourier_transform(matrix_fft)
+            nn.toc(t)
+            vals.append(utf)
+            # if i > 0:
+            #     assert np.allclose(utf, vals[0])
+            # i += 1
