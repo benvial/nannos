c154ec02620580f047b9dc2700d0f5630707e4e6
benvial <b.vial@qmul.ac.uk>
2022-02-05 11:24:40 +0000

    test

--- test/benchmark_fft.py
+++ test/benchmark_fft.py
@@ -8,21 +8,22 @@
 import numpy as np
 import pytest
 
-
 np.random.seed(84)
-N = 2**10
+N = 2 ** 10
 matrix_fft = np.random.rand(N, N) + 1j * np.random.rand(N, N)
+
 
 def test_fft():
     i = 0
     vals, vects = [], []
-    for n in [8,10,12]:
-        N = 2**n
+    for n in [8, 10, 12]:
+        N = 2 ** n
 
         print(f"matrix size = {N}")
         matrix_fft = np.random.rand(N, N) + 1j * np.random.rand(N, N)
         for backend in ["autograd", "jax", "torch", "numpy"]:
             import nannos as nn
+
             print("backend: ", backend)
             nn.set_backend(backend)
             t = nn.tic()
