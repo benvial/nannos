3f2c4c5b23cb24e00a6eb3b4e06ea7f7f9358b5c
Benjamin Vial <b.vial@qmul.ac.uk>
2022-02-15 09:54:13 +0000

    benchmark [skip ci]

--- examples/benchmarks/run.sh
+++ examples/benchmarks/run.sh
@@ -21,6 +21,7 @@ do
   export NUMEXPR_NUM_THREADS=$THREADS
   export XLA_FLAGS='--xla_cpu_multi_thread_eigen=true intra_op_parallelism_threads='$THREADS
   mkdir -p $THREADS
-  taskset -c 1-$THREADS python benchmarks.py $THREADS $1
+  # taskset -c 1-$THREADS python benchmarks.py $THREADS $1
+  python benchmarks.py $THREADS $1
   mv *.npz $THREADS/
 done
