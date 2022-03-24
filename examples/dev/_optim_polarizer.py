60b947f918c42a3271a9bd15b515024f91bccc48
Benjamin Vial <b.vial@qmul.ac.uk>
2022-03-24 18:58:56 +0000

    opt

--- examples/dev/_optim_polarizer.py
+++ examples/dev/_optim_polarizer.py
@@ -12,36 +12,67 @@ Topology optimization
 
 Design of a polarizer
 """
-
-
-import matplotlib as mpl
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 
 import nannos as nn
 
+
+def savefig(dir, base, it):
+    name = dir + "/" + base + str(it).zfill(4) + ".png"
+    plt.savefig(name)
+
+
+def pltpause(interval):
+    backend = plt.rcParams["backend"]
+    if backend in matplotlib.rcsetup.interactive_bk:
+        figManager = matplotlib._pylab_helpers.Gcf.get_active()
+        if figManager is not None:
+            canvas = figManager.canvas
+            if canvas.figure.stale:
+                canvas.draw()
+            canvas.start_event_loop(interval)
+            return
+
+
+def symmetrize_pattern(dens, x=True, y=True, s8=False):
+    dens = bk.array(dens)
+    if x:
+        dens = 0.5 * (dens + bk.fliplr(dens))
+    if y:
+        dens = 0.5 * (dens + bk.flipud(dens))
+    if s8:
+        dens = 0.5 * (dens + dens.T)
+    return dens
+
+
+def format_tensor(T):
+    p = [t.item() for t in T]
+    ps = [f"{_:.3f}" for _ in p]
+    return "  ".join(ps)
+
+
 plt.close("all")
 plt.ion()
-#
-# np.random.seed(1984)
-
 
 #############################################################################
 # Set a backend supporting automatic differentiation
+# backend="autograd"
 
-# nn.set_backend("autograd")
-nn.set_backend("torch")
+
+backend = "torch"
+nn.set_backend(backend)
 # nn.use_gpu(True)
 no = nn.optimize
 bk = nn.backend
 
-
 # ------ RCWA ------
 formulation = "original"
-nh = 51
+nh = 7
 # ------ lattice ------
-L1 = [1, 0]
-L2 = [0, 1]
+L1 = [1.1, 0]
+L2 = [0, 1.1]
 rat_unit_cel = L1[0] / L2[1]
 # ------ angles ------
 theta = 0.0 * bk.pi / 180
@@ -53,25 +84,37 @@ Ny = 2**6
 Npad = 0  # int(Nx / 10)
 Nlayers = 16
 # ------ permittivity ------
-eps_sup = 1
+eps_sup = 1.0
 eps_sub = 1.0
 eps_min = 1.0
-eps_max = 2.7
+# eps_max = 2.7 + 0.0001j
+# eps_min = 2.7 + 0.0001j
+eps_max = 6.5 + 0.0001j
 # ------ thickness ------
-h_ms = 10
+hlays = 0.4
+h_ms = hlays * Nlayers
 # ------ optimization ------
 rfilt = Nx / 50
 maxiter = 20
-stopval = None  # -0.49
+stopval = None  # -0.48
 threshold = (0, 8)
 # ------ objective ------
 order_target = (0, 0)
-Nwl = 3
+alpha_TE = 0.75
+logopt = False
+Nwl = 6
 wls_opt = bk.linspace(2.5, 3.5, Nwl)
+# wls_opt = bk.linspace(3, 3.25, Nwl)
 # ------ initial values ------
-init = "circle"
-rfilt0 = 2 * rfilt  # Nx / 12
-R0 = 0.05
+# init = "circle"
+init = "random"
+init = "lines"
+rfilt0 = 5 * rfilt  # Nx / 12
+R0 = 0.25
+invert0 = True
+normalize0 = True
+filter0 = True
+symz0 = True
 # ------ plot ------
 nax = (
     int(Nlayers**0.5)
@@ -80,6 +123,21 @@ nax = (
 )
 nax_x, nax_y = nax, nax
 figsize = (3, 3)
+
+
+hmin = 0.3
+hmax = 0.3
+amin = 1.0
+amax = 1.0
+
+
+def xtoparams(x):
+    a = x[0]
+    hs = x[1 : Nlayers + 1]
+    dens = x[Nlayers + 1 :]
+    hs = (hmax - hmin) * hs + hmin
+    a = (amax - amin) * a + amin
+    return a, hs, dens
 
 
 def pad(dens, Npad):
@@ -93,6 +151,10 @@ def pad(dens, Npad):
 
 def run(density, proj_level=None, rfilt=0, freq=1, nh=nh, psi=0, nn=nn):
 
+    a, hs, density = xtoparams(density)
+
+    L1 = (a, 0)
+    L2 = (0, a)
     lattice = nn.Lattice((L1, L2))
     pw = nn.PlaneWave(frequency=freq, angles=(theta, phi, psi))
     sup = nn.Layer("Superstrate", epsilon=eps_sup)  # input medium
@@ -104,19 +166,21 @@ def run(density, proj_level=None, rfilt=0, freq=1, nh=nh, psi=0, nn=nn):
 
     density = bk.reshape(density, (Nlayers, Nx, Ny))
     for i, dens in enumerate(density):
+        dens = symmetrize_pattern(dens)
         density_f = no.apply_filter(dens, rfilt)
         density_f = pad(density_f, Npad)
         density_fp = (
             no.project(density_f, proj_level) if proj_level is not None else density_f
         )
         epsgrid = no.simp(density_fp, eps_min, eps_max, p=1)
-        ms = nn.Layer(f"Metasurface_{i}", epsilon=1, thickness=h_ms / Nlayers)
+        ms = nn.Layer(f"Metasurface_{i}", epsilon=1, thickness=hs[i])
         pattern = nn.Pattern(epsgrid, name="design")
         ms.add_pattern(pattern)
         stack.append(ms)
     # stack += [lay, sub]
     stack += [sub]
     sim = nn.Simulation(lattice, stack, pw, nh, formulation=formulation)
+
     return sim
 
 
@@ -124,23 +188,25 @@ def run(density, proj_level=None, rfilt=0, freq=1, nh=nh, psi=0, nn=nn):
 # Define objective function
 
 
-def format_tensor(T):
-    p = [t.item() for t in T]
-    ps = [f"{_:.3f}" for _ in p]
-    return "  ".join(ps)
+fig_obj, ax_obj = plt.subplots(1, 1, figsize=figsize)
 
 
 def fun(density, proj_level, rfilt):
+    global tars_TE
+    global tars_TM
+
     n_jobs = 1
 
     tars_TE = []
     tars_TM = []
 
+    a, hs, _ = xtoparams(density)
+
     @nn.parloop(n_jobs=n_jobs)
     def spec_TE(wl):
         import nannos as nn
 
-        nn.set_backend("torch")
+        nn.set_backend(backend)
         bk = nn.backend
         sim_TE = run(density, proj_level, rfilt, freq=1 / wl, psi=0, nn=nn)
         _, T_TE = sim_TE.diffraction_efficiencies(orders=True)
@@ -151,7 +217,7 @@ def fun(density, proj_level, rfilt):
     def spec_TM(wl):
         import nannos as nn
 
-        nn.set_backend("torch")
+        nn.set_backend(backend)
         bk = nn.backend
         sim_TM = run(density, proj_level, rfilt, freq=1 / wl, psi=nn.pi / 2)
         _, T_TM = sim_TM.diffraction_efficiencies(orders=True)
@@ -174,33 +240,29 @@ def fun(density, proj_level, rfilt):
     #     tar_TM = sim_TM.get_order(T_TM, order_target)
     #     tars_TE.append(tar_TE)
     #     tars_TM.append(tar_TM)
-    try:
-        print("------------------------------------------------")
+
+    if tars_TE[0].grad_fn is not None:
         print("wavelength = ", format_tensor(wls_opt))
         print("T TE       = ", format_tensor(tars_TE))
         print("T TM       = ", format_tensor(tars_TM))
-    except:
-        pass
-    # return (tar_TE-tar_TM)/(tar_TE+tar_TM)
-    # d = (tar_TE) / (tar_TM)
     tar_TE = sum(tars_TE) / Nwl
     tar_TM = sum(tars_TM) / Nwl
-    try:
-        print(f"mean T TE = {float(tar_TE):0.3f}")
-    except:
-        pass
-    try:
-        print(f"mean T TM = {float(tar_TM):0.3f}")
-    except:
-        pass
 
-    alpha = 0.5
-    return tar_TE * alpha - (1 - alpha) * tar_TM
-    # d = (tar_TE) - (tar_TM)
-    # return bk.log(d)
-    # return d
-    # return bk.log(tar_TE* (1-tar_TM))
-    # return bk.log(abs((1-tar_TM))) + tar_TE
+    if tars_TE[0].grad_fn is not None:
+        print(f"mean T TE = {float(tar_TE):0.3f}")
+        print(f"mean T TM = {float(tar_TM):0.3f}")
+
+        print("thicknesses = ", format_tensor(hs))
+        print(f"period      = {a.item():0.3f}")
+
+        print("------------------------------------------------")
+
+    if logopt:
+        out = alpha_TE * bk.log10(tar_TE) - (1 - alpha_TE) * bk.log10(tar_TM)
+    else:
+        out = alpha_TE * tar_TE - (1 - alpha_TE) * tar_TM
+
+    return out
 
 
 ##############################################################################
@@ -210,9 +272,9 @@ def fun(density, proj_level, rfilt):
 def imshow(s, *args, **kwargs):
     extent = (0, rat_unit_cel, 0, 1)
     if nn.DEVICE == "cuda":
-        plt.imshow(s.T.cpu(), *args, extent=extent, **kwargs)
+        plt.imshow(s.T.cpu(), *args, vmin=0, vmax=1, extent=extent, **kwargs)
     else:
-        plt.imshow(s.T, *args, extent=extent, **kwargs)
+        plt.imshow(s.T, *args, vmin=0, vmax=1, extent=extent, **kwargs)
 
 
 densities0 = []
@@ -220,28 +282,30 @@ densities_plot0 = []
 
 for i in range(Nlayers):
 
+    x, y = bk.linspace(0, 1, Nx), bk.linspace(0, 1, Ny)
+    x, y = bk.meshgrid(x, y)
+    density0 = bk.ones((Nx, Ny))
     if init == "random":
 
         density0rand = np.random.rand(Nx, Ny)
         # random
-        density0 = bk.array(density0rand)
-        density0 = 0.5 * (density0 + bk.fliplr(density0))
-        density0 = 0.5 * (density0 + bk.flipud(density0))
-        density0 = 0.5 * (density0 + density0.T)
-        density0 = no.apply_filter(density0, rfilt0)
-        density0 = (density0 - density0.min()) / (density0.max() - density0.min())
+        # density0 = bk.array(density0rand)
+        # density0 = 0.5 * (density0 + bk.fliplr(density0))
+        # density0 = 0.5 * (density0 + bk.flipud(density0))
+        # density0 = 0.5 * (density0 + density0.T)
+        density0 = symmetrize_pattern(density0rand)
     elif init == "circle":
-        #
         ### circle
-        x, y = bk.linspace(0, 1, Nx), bk.linspace(0, 1, Ny)
-        x, y = bk.meshgrid(x, y)
-        density0 = bk.ones((Nx, Ny))
-
         density0[(x - 0.5) ** 2 + (y - 0.5) ** 2 < R0**2] = 0
-        density0 = no.apply_filter(density0, rfilt0)
-        density0 = (density0 - density0.min()) / (density0.max() - density0.min())
-        density0 *= 0.5
-        # density0 = 1-density0
+        # density0 *= 0.5
+
+    elif init == "lines":
+        if i % 2:
+            density0[abs(x - 0.5) < R0] = 0
+        else:
+            density0[abs(y - 0.5) < R0] = 0
+
+        density0 += 0.0001 * np.random.rand(Nx, Ny)
 
     else:
 
@@ -249,11 +313,30 @@ for i in range(Nlayers):
         ## need to add random because if uniform gradient is NaN with torch  (MKL error)
         density0 = bk.ones((Nx, Ny)) * 0.5 + 0.0001 * np.random.rand(Nx, Ny)
 
+    if filter0:
+        density0 = no.apply_filter(density0, rfilt0)
+
+    if normalize0:
+        density0 = (density0 - density0.min()) / (density0.max() - density0.min())
+
+    if invert0:
+        density0 = 1 - density0
+
     density0 = density0.flatten()
     density_plot0 = bk.reshape(density0, (Nx, Ny))
 
     densities0.append(density0)
     densities_plot0.append(density_plot0)
+
+if symz0:
+    densities0 = bk.reshape(bk.hstack(densities0), (Nlayers, Nx, Ny))
+    densities0[int(Nlayers / 2) :] = bk.flipud(densities0[: -int(Nlayers / 2)])
+    densities0 = list(densities0.ravel())
+
+
+geompar0 = bk.array(np.random.rand(Nlayers + 1))
+
+densities0 = [geompar0] + densities0
 
 density0 = bk.hstack(densities0)
 #
@@ -281,10 +364,13 @@ ax = ax.flatten()
 def callback(x, y, proj_level, rfilt):
     global it
     print(f"iteration {it}")
-    density = bk.reshape(x, (Nlayers, Nx, Ny))
+    a, hs, density = xtoparams(x)
+
+    density = bk.reshape(density, (Nlayers, Nx, Ny))
 
     for i, dens in enumerate(density):
         a = ax[i]
+        dens = symmetrize_pattern(dens)
         density_f = no.apply_filter(dens, rfilt)
 
         density_f = pad(density_f, Npad)
@@ -299,12 +385,42 @@ def callback(x, y, proj_level, rfilt):
         a.axis("off")
     plt.suptitle(f"iteration {it}, objective = {y:.5f}")
     # plt.tight_layout()
-    plt.pause(0.1)
+    pltpause(0.1)
+
+    savefig("/home/ben/Code/dev/prod/nannos/polarizer", "dens", it)
+
+    # ---------
+
+    nlines = 5
+    if it > 0:
+        for line in ax_obj.get_lines()[-2 * nlines :]:
+            line.set_alpha(0.04)
+        for line in ax_obj.get_lines()[: -2 * nlines]:
+            line.remove()
+
+    # ax_obj.clear()
+    plt.sca(ax_obj)
+    (line_TE,) = ax_obj.plot(wls_opt, bk.array(tars_TE), "o-r")
+    (line_TM,) = ax_obj.plot(wls_opt, bk.array(tars_TM), "s-b")
+    ax_obj.legend([line_TE, line_TM], ["TE", "TM"], loc="lower left")
+    ax_obj.set_ylim(-0.1, 1.1)
+    ax_obj.set_xlabel("Wavelength (mm)")
+    ax_obj.set_ylabel("Transmission")
+    pltpause(0.1)
+    savefig("/home/ben/Code/dev/prod/nannos/polarizer", "obj", it)
+
     it += 1
 
 
 ##############################################################################
 # Create `TopologyOptimizer` object
+
+
+# x_opt = np.load("xopt.npz",allow_pickle=True)["x_opt"]
+# density0 = x_opt
+
+# threshold = (0,1)
+# maxiter = 40
 
 opt = no.TopologyOptimizer(
     fun,
@@ -321,9 +437,12 @@ opt = no.TopologyOptimizer(
 ##############################################################################
 # Run the optimization
 
-density_opt, f_opt = opt.minimize()
+x_opt, f_opt = opt.minimize()
 # density_opt , f_opt= density0,0
 
+# np.savez("xopt.npz",x_opt=x_opt)
+# xsx
+a, hs, density_opt = xtoparams(x_opt)
 
 ##############################################################################
 # Postprocess to get a binary design
@@ -333,7 +452,8 @@ density_bins = []
 density_opt = bk.reshape(bk.array(density_opt), (Nlayers, Nx, Ny))
 proj_level = 2 ** (opt.threshold[-1] - 1)
 for i in range(Nlayers):
-    density_optf = no.apply_filter(density_opt[i], rfilt)
+    dens = symmetrize_pattern(density_opt[i])
+    density_optf = no.apply_filter(dens, rfilt)
     density_optf = pad(density_optf, Npad)
     density_optfp = no.project(density_optf, proj_level)
     density_bin = bk.ones_like(density_optfp)
@@ -370,7 +490,10 @@ for i, dens in enumerate(density_bin_plot):
     # plt.colorbar()
 plt.suptitle(f"iteration {it}, objective = {f_opt:.5f}")
 
-sim = run(density_bin, None, 0)
+x_opt_bin = bk.array(x_opt.copy())
+x_opt_bin[Nlayers + 1 :] = density_bin
+
+sim = run(x_opt_bin, None, 0)
 
 
 ##############################################################################
@@ -459,10 +582,10 @@ def plot_struc(sim, p, nper=(1, 1), dz=0.0, opacity=1):
     return p
 
 
-#
-p = pv.Plotter()
-p = plot_struc(sim, p, nper=(5, 5), dz=0, opacity=1)
-p.show()
+# #
+# p = pv.Plotter()
+# p = plot_struc(sim, p, nper=(5, 5), dz=0, opacity=1)
+# p.show()
 
 
 ##############################################################################
@@ -475,11 +598,11 @@ wls = np.linspace(2.0, 4, 500)
 def spec(wl):
     import nannos as nn
 
-    nn.set_backend("torch")
+    nn.set_backend(backend)
     bk = nn.backend
     TT = []
     for psi in [0, nn.pi / 2]:
-        sim = run(density_bin, None, 0, freq=1 / wl, psi=psi, nh=51, nn=nn)
+        sim = run(x_opt_bin, None, 0, freq=1 / wl, psi=psi, nh=51, nn=nn)
         R, T = sim.diffraction_efficiencies(orders=True)
         print("Σ R = ", float(sum(R)))
         print("Σ T = ", float(sum(T)))
