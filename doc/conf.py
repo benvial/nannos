54889ad53e5423b45f3bc1302bbeec1a2f42ab71
Benjamin Vial <b.vial@qmul.ac.uk>
2022-06-13 13:23:43 +0100

    doc

--- doc/conf.py
+++ doc/conf.py
@@ -140,7 +140,7 @@ release = package.__version__  # + '-git'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -190,8 +190,10 @@ def setup(app):
 html_theme_options = {
     "show_toc_level": 3,
     "show_prev_next": False,
+    "show_nav_level": 2,
     "navbar_end": ["navbar-icon-links"],
 }
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
