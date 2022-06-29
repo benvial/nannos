5efae13ad2785c58b8f5b45ba3336035ad112af2
benvial <b.vial@qmul.ac.uk>
2022-06-29 16:44:20 +0100

    Publish v2.1.0

---
+++ test/conda_test.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Benjamin Vial
+# This file is part of nannos
+# License: GPLv3
+# See the documentation at nannos.gitlab.io
+
+import nannos as nn
+
+
+def test_simulation():
+    backend = nn.get_backend()
+    print(backend)
+    lattice = nn.Lattice(basis_vectors=[[1.0, 0], [0, 1.0]], discretization=2**9)
+    sup = lattice.Layer("Superstrate", epsilon=1)
+    ms = lattice.Layer("Metasurface", thickness=0.5)
+    sub = lattice.Layer("Substrate", epsilon=2)
+    ms.epsilon = lattice.ones() * 12.0
+    circ = lattice.circle(center=(0.5, 0.5), radius=0.2)
+    if backend == "jax":
+        ms.epsilon = ms.epsilon.at[circ].set(1)
+    else:
+        ms.epsilon[circ] = 1
+    stack = [sup, ms, sub]
+    pw = nn.PlaneWave(wavelength=0.9, angles=(0, 0, 0))
+    sim = nn.Simulation(stack, pw, nh=200)
+    R, T = sim.diffraction_efficiencies()
+    assert nn.backend.allclose(R + T, 1)
