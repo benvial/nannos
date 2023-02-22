f958bd30ba8e6a16dd8a5535a3302983e0c25be8
Benjamin Vial <benjamin.vial84@gmail.com>
2023-02-22 15:28:12 +0000

    dev [skip ci]

--- examples/benchmarks/plot_kaggle.py
+++ examples/benchmarks/plot_kaggle.py
@@ -8,7 +8,7 @@
 Benchmarks
 ==========
 
-Backend performace comparison.
+Backend performace comparison, with GPU acceleration, as run on Kaggle (https://www.kaggle.com/code/benjaminvial/nannos_cpu)
 """
 
 import os
@@ -16,37 +16,60 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 
+plt.ion()
+
 ##############################################################################
 # Load data
 absolute_path = os.path.dirname("__file__")
-full_path = os.path.join(absolute_path, "kaggle/nannos/results_kaggle.npz")
-
+full_path = os.path.join(absolute_path, "kaggle/nannos_cpu/results_kaggle_cpu.npz")
 arch = np.load(full_path, allow_pickle=True)
 num_harmo = arch["num_harmo_real"]
-timings_gpu_torch = arch["timings_gpu_torch"]
-timings_cpu_torch = arch["timings_cpu_torch"]
-timings_numpy = arch["timings_numpy"]
+timings = arch["timedict"].item()
+# cases = arch["cases"].tolist()
+
+
+full_path = os.path.join(absolute_path, "kaggle/nannos_gpu/results_kaggle_gpu.npz")
+arch = np.load(full_path, allow_pickle=True)
+timings.update(arch["timedict"].item())
+# cases += arch["cases"].tolist()
+
+skip_first = True
+
+ifirst = 1 if skip_first else 0
+cases = ["numpy", "scipy", "autograd", "torch cpu", "torch gpu", "jax cpu", "jax gpu"]
+
 
 ##############################################################################
 # Time vs. number of harmonics
 # ------------------------------
+tav = dict()
+tstd = dict()
+for case in cases:
+    t = np.array(timings[case])[:, ifirst:]
+    tav[case] = np.mean(t, axis=1)
+    tstd[case] = np.std(t, axis=1)
 
-tav_gpu_torch = np.array([h.average for h in timings_gpu_torch])
-tstd_gpu_torch = np.array([h.stdev for h in timings_gpu_torch])
-t_gpu_torch = np.array([h.all_runs for h in timings_gpu_torch])
-tav_cpu_torch = np.array([h.average for h in timings_cpu_torch])
-tstd_cpu_torch = np.array([h.stdev for h in timings_cpu_torch])
-t_cpu_torch = np.array([h.all_runs for h in timings_cpu_torch])
-tav_numpy = np.array([h.average for h in timings_numpy])
-tstd_numpy = np.array([h.stdev for h in timings_numpy])
-t_numpy = np.array([h.all_runs for h in timings_numpy])
+colors = plt.rcParams["axes.prop_cycle"].by_key()["color"]
+
+colors = colors[:3] + [colors[4], colors[4]] + [colors[5], colors[5]]
 
 plt.figure()
-plt.errorbar(num_harmo, tav_gpu_torch, yerr=tstd_gpu_torch, label="torch GPU")
-plt.errorbar(num_harmo, tav_cpu_torch, yerr=tstd_cpu_torch, label="torch CPU")
-plt.errorbar(num_harmo, tav_numpy, yerr=tstd_numpy, label="numpy")
+for i, case in enumerate(cases):
+    ls = "--" if case.split(" ")[-1] == "gpu" else "-"
+    plt.errorbar(
+        num_harmo,
+        tav[case],
+        yerr=tstd[case],
+        label=case,
+        ls=ls,
+        color=colors[i],
+        fmt="s",
+        capsize=5,
+    )
+
 plt.xlabel("Number of harmonics")
 plt.ylabel("CPU time (s)")
+plt.xscale("log")
 plt.yscale("log")
 plt.legend()
 plt.tight_layout()
@@ -55,17 +78,28 @@ plt.tight_layout()
 # Speedup vs. number of harmonics
 # -----------------------------------
 
-speedup_gpu = t_numpy / t_gpu_torch
-speedup_gpu_av = np.mean(speedup_gpu, axis=1)
-speedup_gpu_std = np.std(speedup_gpu, axis=1)
-speedup_cpu = t_numpy / t_cpu_torch
-speedup_cpu_av = np.mean(speedup_cpu, axis=1)
-speedup_cpu_std = np.std(speedup_cpu, axis=1)
+speedup_av = dict()
+speedup_std = dict()
+
+for case in cases[1:]:
+    s = np.array(timings["numpy"])[:, ifirst:] / np.array(timings[case])[:, ifirst:]
+    speedup_av[case] = np.mean(s, axis=1)
+    speedup_std[case] = np.std(s, axis=1)
 
 
 plt.figure()
-plt.errorbar(num_harmo, speedup_gpu_av, yerr=speedup_gpu_std, label="torch GPU")
-plt.errorbar(num_harmo, speedup_cpu_av, yerr=speedup_cpu_std, label="torch CPU")
+for i, case in enumerate(cases[1:]):
+    ls = "--" if case.split(" ")[-1] == "gpu" else "-"
+    plt.errorbar(
+        num_harmo,
+        speedup_av[case],
+        yerr=speedup_std[case],
+        label=case,
+        ls=ls,
+        color=colors[i + 1],
+        fmt="s",
+        capsize=5,
+    )
 plt.xlabel("number of harmonics")
 plt.ylabel("Speedup vs. numpy")
 plt.legend()
